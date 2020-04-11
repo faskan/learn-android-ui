87c8d0f654fca515460ee73227e502d96721bac1
Faisal Khan <faisalkhanthayub@gmail.com>
2020-04-12 00:35:16 +0200

    save instance state

--- app/src/main/java/org/faskan/serversocketapplication/MainActivity.java
+++ app/src/main/java/org/faskan/serversocketapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package org.faskan.serversocketapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,16 +13,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     public static final String MAIN_MESSAGE = "MESSAGE";
     private int mCount;
     private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("MainActivity", "Hello World");
+        Log.d(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = findViewById(R.id.text_count);
+        if(savedInstanceState != null) {
+            String savedText = savedInstanceState.getString("count");
+            mShowCount.setText(savedText);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(LOG_TAG, "onStart");
+        super.onStart();
     }
 
     public void showToast(View view) {
@@ -36,7 +49,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        intent.putExtra(MAIN_MESSAGE, ((EditText)findViewById(R.id.editText_msgToSecondActivity)).getText().toString());
+        intent.putExtra(MAIN_MESSAGE, ((EditText) findViewById(R.id.editText_msgToSecondActivity)).getText().toString());
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("count",mShowCount.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
--- app/src/main/java/org/faskan/serversocketapplication/SecondActivity.java
+++ app/src/main/java/org/faskan/serversocketapplication/SecondActivity.java
@@ -3,15 +3,55 @@ package org.faskan.serversocketapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         String stringExtra = getIntent().getStringExtra(MainActivity.MAIN_MESSAGE);
         ((TextView) findViewById(R.id.textView_secondActivity)).setText(stringExtra);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(LOG_TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
